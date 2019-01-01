ecfff6f479115cf02164517ce8a74b8a74f34ce8
Amin <moradi.amin74@gmail.com>
2019-01-01 23:22:57 +0000

    first commit

---
+++ resize.py
@@ -0,0 +1,13 @@
+# resize doggoGan.py
+import os
+import cv2
+
+src = "./data"
+dst = "./resizedData" # resized
+
+os.mkdir(dst)
+
+for each in os.listdir(src):
+    img = cv2.imread(os.path.join(src,each))
+    img = cv2.resize(img,(256,256))
+    cv2.imwrite(os.path.join(dst,each), img)
